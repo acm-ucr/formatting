8cc9263641282be47ef8fccf8ceedd42d433fe6e
Divyank Shah <divyank.shah.2016@gmail.com>
2023-12-18 02:22:46 -0800

    i forgot to build

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
+import { getInput, setOutput, setFailed } from "@actions/core";
+import { payload } from "@actions/github";
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
+  const nameToGreet = getInput("who-to-greet");
   console.log(`Hello ${nameToGreet}!`);
   const time = new Date().toTimeString();
-  core.setOutput("time", time);
-  console.log(`The event payload: ${JSON.stringify(github.payload.base)}`);
+  setOutput("time", time);
+  console.log(`The event payload: ${JSON.stringify(payload.base)}`);
 } catch (error) {
-  core.setFailed(error.message);
+  setFailed(error.message);
 }
