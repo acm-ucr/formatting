fb485e9bc10b81bc3b6c0d9aa52438257522128d
Divyank Shah <divyank.shah.2016@gmail.com>
2023-12-18 05:06:56 -0800

    cleanup

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-import { getInput, setFailed, info } from "@actions/core";
-import { context, getOctokit } from "@actions/github";
+import { setFailed, info } from "@actions/core";
 import { getExecOutput } from "@actions/exec";
 
-const Prettier = async () => {
-  const token = getInput("token");
-
-  if (!token)
-    setFailed(
-      "There is no Github Token provided. Please refer to the Prettier-Action Documentation for reference."
-    );
-
+const prettier = async () => {
   const { stdout } = await getExecOutput("npm i prettier -D");
   info(stdout);
 
@@ -17,21 +9,8 @@ const Prettier = async () => {
     const { stdout } = await getExecOutput("npx prettier --check .");
     info(stdout);
   } catch (err) {
-    const octokit = getOctokit(token);
-
-    const name = context.payload.repository.name;
-    const number = context.payload.repository.number;
-    const owner = context.payload.repository.owner.login;
-
-    await octokit.rest.pulls.createReview({
-      owner: owner,
-      repo: name,
-      pull_number: number,
-      event: "REQUEST_CHANGES",
-      body: "The code is not formatted correctly, please format your code and push your changes again.",
-    });
     setFailed("Your code is not formatted correctly. Please format your code.");
   }
 };
 
-await Prettier();
+await prettier();
