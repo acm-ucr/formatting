be3ac68039799cb125cc83c3daa2104eecc9e08f
Divyank Shah <divyank.shah.2016@gmail.com>
2023-12-18 02:10:50 -0800

    add git url

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
+import { core } from "@actions/core";
+import { context } from "@actions/github";
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -7,9 +7,12 @@ try {
   console.log(`Hello ${nameToGreet}!`);
   const time = new Date().toTimeString();
   core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  const payload = JSON.stringify(context.payload, undefined, 2);
+  console.log(
+    `The event payload: ${payload} ${JSON.stringify(
+      context.payload.base.repo.git_url
+    )}`
+  );
 } catch (error) {
   core.setFailed(error.message);
 }
