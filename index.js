09e4bc7b5f7b37e492794a629dbabcbe9c3bd1c5
Divyank Shah <divyank.shah.2016@gmail.com>
2023-12-18 05:17:13 -0800

    log payload

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 import { setFailed, info } from "@actions/core";
-import { getExecOutput } from "@actions/exec";
+import { exec, getExecOutput } from "@actions/exec";
+import { context } from "@actions/github";
 
 const prettier = async () => {
+  const token = core.getInput("token");
+
+  if (!token)
+    setFailed("No available token. Please provide the GITHUB_TOKEN variable");
+
   const { stdout } = await getExecOutput("npm i prettier -D");
   info(stdout);
+
+  console.log(context.payload);
 
   try {
     const { stdout } = await getExecOutput("npx prettier --check .");
     info(stdout);
   } catch (err) {
+    // await exec("npx prettier --write .");
+    // await exec(`git config --global user.name "your username"`);
+    // await exec(`git config --global user.email "your email"`);
+    // await exec("git add .");
     setFailed("Your code is not formatted correctly. Please format your code.");
   }
 };
