a42a5ea8d58d4067e84ea9ab22ed2c6dcd28fdb2
Divyank Shah <divyank.shah.2016@gmail.com>
2023-12-18 02:16:30 -0800

    update

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ try {
   console.log(`Hello ${nameToGreet}!`);
   const time = new Date().toTimeString();
   core.setOutput("time", time);
-  console.log(
-    `The event payload: ${JSON.stringify(context.payload.base.repo.git_url)}`
-  );
+  console.log(`The event payload: ${JSON.stringify(context.payload.base)}`);
 } catch (error) {
   core.setFailed(error.message);
 }
