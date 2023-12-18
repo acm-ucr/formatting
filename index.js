8beaed86faebaf8b886d09249731105f06dd3590
Divyank Shah <divyank.shah.2016@gmail.com>
2023-12-18 02:19:49 -0800

    default

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import { getInput } from "@actions/core";
-import { payload } from "@actions/github";
+import core from "@actions/core";
+import context from "@actions/github";
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = getInput("who-to-greet");
+  const nameToGreet = core.getInput("who-to-greet");
   console.log(`Hello ${nameToGreet}!`);
   const time = new Date().toTimeString();
   core.setOutput("time", time);
-  console.log(`The event payload: ${JSON.stringify(payload.base)}`);
+  console.log(`The event payload: ${JSON.stringify(context.payload.base)}`);
 } catch (error) {
   core.setFailed(error.message);
 }
