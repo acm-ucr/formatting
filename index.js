6a7a5fa226be79c3c67edf10c1a3e313d2c1f4fa
Divyank Shah <divyank.shah.2016@gmail.com>
2023-12-18 02:14:28 -0800

    change console log

--- index.js
+++ index.js
@@ -7,11 +7,8 @@ try {
   console.log(`Hello ${nameToGreet}!`);
   const time = new Date().toTimeString();
   core.setOutput("time", time);
-  const payload = JSON.stringify(context.payload, undefined, 2);
   console.log(
-    `The event payload: ${payload} ${JSON.stringify(
-      context.payload.base.repo.git_url
-    )}`
+    `The event payload: ${JSON.stringify(context.payload.base.repo.git_url)}`
   );
 } catch (error) {
   core.setFailed(error.message);
